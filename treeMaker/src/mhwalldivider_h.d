c9aa125a345cb2f860200ce4788ed2b073ec0cab
Tatyana Mamontova <taya-violet@yandex.ru>
2017-11-21 15:21:08 +0100

    Delete mhwalldivider_h.d

--- treeMaker/src/mhwalldivider_h.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./mhwalldivider_h.so: /cvmfs/hades.gsi.de/install/root-5.34.34/include/cintdictversion.h /cvmfs/hades.gsi.de/install/root-5.34.34/include/RVersion.h
-mhwalldivider_h__ROOTBUILDVERSION= 5.34/34
