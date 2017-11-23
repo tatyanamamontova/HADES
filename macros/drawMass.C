b477b4db0dd6b3e0821e961d030a0e06e8556d12
Татьяна Мамонтова <truth_quark@MacBook-Pro-Tatana.local>
2017-11-23 12:46:41 +0100

    changes

--- macros/drawMass.C
+++ macros/drawMass.C
@@ -18,22 +18,29 @@ void drawMass(char* input){
     ch->Add(trees);
 
     TH2F *hMTheta = new TH2F("hMTheta", "Mass vs Theta; m [Mev/c^2]; #theta[rad]; nTracks", 1000, 0, 4000, 100, 0, 1.6);
+    TH2F *hPhiTheta = new TH2F ("hPhiTheta", "Phi vs Theta; #phi [rad]; #theta [rad]; nTracks", 100, 0, 6.2, 100, 0, 1.6);
 
     Short_t nTracks;
     const Short_t maxNTracks = 200;
     Float_t pt[maxNTracks];
     Float_t theta[maxNTracks];
     Float_t mass[maxNTracks];
+    Float_t phi[maxNTracks];
 
     ch->SetBranchAddress("pt", pt);
     ch->SetBranchAddress("mass", mass);
     ch->SetBranchAddress("theta", theta);
     ch->SetBranchAddress("nTracks", &nTracks);
+    ch->SetBranchAddress("phi", phi);
+
 
     int entry=0;
     while(entry < ch->GetEntries()) {
         ch->GetEntry(entry);
-        for (Int_t i = 0; i< nTracks; i++) hMTheta->Fill(mass[i], theta[i]);
+        for (Int_t i = 0; i< nTracks; i++) {
+            hMTheta->Fill(mass[i], theta[i]);
+            hPhiTheta->Fill(phi[i], theta[i]);
+        }
         entry++;
 }
 
@@ -49,5 +56,14 @@ void drawMass(char* input){
     delete canv;
     delete leg;
 
- 
+    canv = new TCanvas();
+    leg = new TLegend(0.7,0.7,0.9,0.9);
+    hPhiTheta->Draw("colz");
+    hPhiTheta->SetStats(false);
+    sprintf(picName, "./pics/PhiVsTheta.png");
+    canv->SaveAs(picName);
+    sprintf(picName, "./pics/PhiVsTheta.C");
+    canv->SaveAs(picName);
+    delete canv;
+    delete leg;
 }
