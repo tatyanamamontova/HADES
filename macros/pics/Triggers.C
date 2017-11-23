3ab84b0405de923b864b6504ba8b6c48fb1723dd
TatianaMamontova <tmamont@lxbk0199.gsi.de>
2017-11-23 12:41:17 +0100

    pics

--- macros/pics/Triggers.C
+++ macros/pics/Triggers.C
@@ -1,20 +1,20 @@
 {
 //=========Macro generated from canvas: c1/c1
-//=========  (Thu Nov 23 11:57:11 2017) by ROOT version5.34/34
+//=========  (Thu Nov 23 12:39:17 2017) by ROOT version5.34/34
    TCanvas *c1 = new TCanvas("c1", "c1",0,0,700,500);
-   c1->Range(-0.5,-82662.17,4.5,743959.5);
+   c1->Range(-0.625,-68174.67,5.625,613572);
    c1->SetFillColor(0);
    c1->SetBorderMode(0);
    c1->SetBorderSize(2);
    c1->SetFrameBorderMode(0);
    c1->SetFrameBorderMode(0);
    
-   TH1F *hCuts = new TH1F("hCuts","Event cuts",4,0,4);
-   hCuts->SetBinContent(2,629807);
-   hCuts->SetBinContent(3,629807);
-   hCuts->SetBinContent(4,629807);
+   TH1F *hCuts = new TH1F("hCuts","Event cuts",5,0,5);
+   hCuts->SetBinContent(2,174579);
+   hCuts->SetBinContent(3,519426);
+   hCuts->SetBinContent(5,336);
    hCuts->SetBarWidth(0.9);
-   hCuts->SetEntries(1889421);
+   hCuts->SetEntries(694341);
    hCuts->SetStats(0);
    hCuts->SetFillColor(38);
 
@@ -27,6 +27,7 @@
    hCuts->GetXaxis()->SetBinLabel(2,"PT2");
    hCuts->GetXaxis()->SetBinLabel(3,"PT3");
    hCuts->GetXaxis()->SetBinLabel(4,"PT4");
+   hCuts->GetXaxis()->SetBinLabel(5,"No one");
    hCuts->GetXaxis()->SetLabelFont(42);
    hCuts->GetXaxis()->SetLabelSize(0.035);
    hCuts->GetXaxis()->SetTitleSize(0.035);
