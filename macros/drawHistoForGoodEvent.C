37adafe25d5630d06d5b31f1ee91b3e3e9e10298
Татьяна Мамонтова <truth_quark@MacBook-Pro-Tatana.local>
2017-11-24 16:18:10 +0100

    updates

--- macros/drawHistoForGoodEvent.C
+++ macros/drawHistoForGoodEvent.C
@@ -4,17 +4,20 @@
 //@param tree without .root
 //@return pics with histos
 
-#include "TTree.h"
+#include <TFile.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TColor.h>
+#include <TH1F.h>
+#include <TH2F.h>
 #include <iostream>
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TF1.h"
-#include <TNtuple.h>
-#include "TFile.h"
-#include "TChain.h"
 
-void drawHistoForGoodEvent(char* input) {
+void DrawCuts(char* input) {
 
+TCanvas* canv;
+TLegend* leg;
 TChain *ch = new TChain;
 char picName[200];
 char trees[200];
@@ -28,6 +31,7 @@ ch->SetBranchAddress("cuts", cuts);
 
 TH1F* hCuts = new TH1F("hCuts", "Event cuts; ; Events", nCuts, 0, nCuts);
 TH1F* hisgoodEvent = new TH1F("hisgoodEvent", "All cuts; ; Events", 2, -0.5, 1.5);
+TH1F* hCutRejected = new TH1F("hCutRejected", "Rejected Events; ; Events", nCuts, 0, nCuts);
 
 // ch->SetBranchAddress("trigger",             &trigger);
 // ch->SetBranchAddress("vertexClust",         &vertexClust);
@@ -41,19 +45,34 @@ TH1F* hisgoodEvent = new TH1F("hisgoodEvent", "All cuts; ; Events", 2, -0.5, 1.5
 
 
 int entry=0;
-int count;
-
+// Bool_t check;
 while(entry < ch->GetEntries()) {
     ch->GetEntry(entry);
-    count = 0;
+
+    // check = kTRUE;
     for (Int_t i = 0; i < nCuts; i++){
         if(cuts[i]) {
             hCuts->Fill(i+0.5);
-            count++;
+        }
+        else {
+            hisgoodEvent->Fill(1);
         }
 
+        // for (Int_t j = 0; j < nCuts; j++){
+        //     if (i != j) check = check && cuts[j];
+        //     else check = check && !cuts[j];
+        // }
     }
-    if (count == nCuts) hisgoodEvent->Fill(1);
+        // if (check) hCutRejected ->Fill(i+0.5);
+        if (!cuts[0]&&cuts[1]&&cuts[2]&&cuts[3]&&cuts[4]&&cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(0.5);
+        if (cuts[0]&&!cuts[1]&&cuts[2]&&cuts[3]&&cuts[4]&&cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(1.5);
+        if (cuts[0]&&cuts[1]&&!cuts[2]&&cuts[3]&&cuts[4]&&cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(2.5);
+        if (cuts[0]&&cuts[1]&&cuts[2]&&!cuts[3]&&cuts[4]&&cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(3.5);
+        if (cuts[0]&&cuts[1]&&cuts[2]&&cuts[3]&&!cuts[4]&&cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(4.5);
+        if (cuts[0]&&cuts[1]&&cuts[2]&&cuts[3]&&cuts[4]&&!cuts[5]&&cuts[6]&&cuts[7]) hCutRejected ->Fill(5.5);
+        if (cuts[0]&&cuts[1]&&cuts[2]&&cuts[3]&&cuts[4]&&cuts[5]&&!cuts[6]&&cuts[7]) hCutRejected ->Fill(6.5);
+        if (cuts[0]&&cuts[1]&&cuts[2]&&cuts[3]&&cuts[4]&&cuts[5]&&cuts[6]&&!cuts[7]) hCutRejected ->Fill(7.5);
+
     hisgoodEvent->Fill(0);
     entry++;
 }
@@ -70,9 +89,9 @@ hCuts->SetFillColor(38);
 hCuts->SetBarWidth(0.9);
 hCuts->SetStats(0);
 hCuts->Draw("b");
-sprintf(picName, "../results/Cuts.png");
+sprintf(picName, "./pics/Cuts.png");
 canv->SaveAs(picName);
-sprintf(picName, "../results/Cuts.C");
+sprintf(picName, "./pics/Cuts.C");
 canv->SaveAs(picName);
 delete canv;
 delete leg;
@@ -89,12 +108,29 @@ hisgoodEvent->SetBarWidth(0.9);
 hisgoodEvent->SetStats(0);
 hisgoodEvent->Draw("b");
 hisgoodEvent->Draw("b");
-sprintf(picName, "../results/Evt.png");
+sprintf(picName, "./pics/Evt.png");
 canv->SaveAs(picName);
-sprintf(picName, "../results/Evt.C");
+sprintf(picName, "./pics/Evt.C");
 canv->SaveAs(picName);
 delete canv;
 delete leg;
+
+canv = new TCanvas();
+leg = new TLegend(0.7,0.7,0.9,0.9);
+for (Int_t i=0; i < nCuts; i++) { 
+    hCutRejected->GetXaxis()->SetBinLabel(i+1,cutName[i].c_str());
+}
+hCutRejected->SetFillColor(38);
+hCutRejected->SetBarWidth(0.9);
+hCutRejected->SetStats(0);
+hCutRejected->Draw("b");
+sprintf(picName, "./pics/Cuts_rej.png");
+canv->SaveAs(picName);
+sprintf(picName, "./pics/Cuts_rej.C");
+canv->SaveAs(picName);
+delete canv;
+delete leg;
+
 
 
 }
